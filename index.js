b8ca24d60836c9f38d47c12758a251db7852c598
robpwatkins <robpwatkins@gmail.com>
2021-12-28 10:30:58 -0600

    got one lighting up

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const pads = document.querySelectorAll('.pad');
+const sequence = [];
 
-pads.forEach(pad => pad.addEventListener('click', handlePadClick));
+window.onload = function() {
+  startGame();
+};
+
+pads.forEach(pad => {
+  pad.addEventListener('click', handlePadClick)
+  pad.addEventListener('transitionend', handleTransitionEnd);
+});
 
 function handlePadClick() {
   const { number } = this.dataset;
   console.log('number: ', number);
+};
+
+function handleTransitionEnd() {
+  console.log('heyoo');
+}
+
+function startGame() {
+  sequence.push(1);
+  sequence.forEach(number => {
+    const pad = document.querySelector(`[data-number='${number}']`);
+    pad.classList.add('active');
+    setTimeout(() => pad.classList.remove('active'), 250);
+  })
 };
