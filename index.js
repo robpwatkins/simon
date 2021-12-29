22c2d0e31aba47a522d90ecfe7a8066d3b95cb79
robpwatkins <robpwatkins@gmail.com>
2021-12-29 07:44:35 -0600

    wigging out a lil

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const pads = document.querySelectorAll('.pad');
 const sequence = [];
+let sequenceIdx = 0;
 
 document.querySelector('.inner-circle').addEventListener('click', playSequence);
 
@@ -9,10 +10,16 @@ pads.forEach(pad => {
 });
 
 function handlePadClick() {
+  console.log('heyoo');
   const { number } = this.dataset;
   this.classList.add('active');
-  if (checkSequence(number)) return playSequence();
-  console.log('Game over!');
+  while (sequenceIdx < sequence.length) {
+    console.log('sequence: ', sequence);
+    console.log('number: ', number);
+    if (number != sequence[sequenceIdx]) return gameOver();
+    sequenceIdx++;
+    playSequence();
+  }
 };
 
 function checkSequence(number) {
@@ -27,7 +34,6 @@ function handleTransitionEnd() {
 function playSequence() {
   const randomNum = Math.floor(Math.random() * (4 - 1 + 1) + 1);
   sequence.push(randomNum);
-  console.log('sequence: ', sequence);
   sequence.forEach((number, index) => {
     setTimeout(() => {
       const pad = document.querySelector(`[data-number='${number}']`);
@@ -35,3 +41,8 @@ function playSequence() {
     }, index * 1000)
   })
 };
+
+function gameOver() {
+  sequence.splice();
+  console.log('Game over!');
+}
