13704134ed8538653b9e5936fc530388105471bb
robpwatkins <robpwatkins@gmail.com>
2022-01-09 20:54:50 -0600

    gonna figure it out later

--- index.js
+++ index.js
@@ -8,60 +8,56 @@ const fxSwitch = document.querySelector('.fx-switch');
 const fx = document.querySelector('#fx');
 let fxEnabled = true;
 let gameStarted = false;
-var context;
-var saved;
+let context;
+let saved;
+let source;
 
 try {
-    context = new (window.AudioContext || window.webkitAudioContext)();
+
 }
 catch (e) {
-    console.log("Your browser doesn't support Web Audio API");
+  console.log("Your browser doesn't support Web Audio API");
 }
 
-if (saved) {
-    playSound(saved);
-} else {
-    loadSound();
-}
+// if (saved) {
+//   playSound(saved);
+// } else {
+//   loadSound();
+// }
 
-//loading sound into the created audio context
 function loadSound() {
-    //set the audio file's URL
-    var audioURL = 'https://github.com/robpwatkins/simon/blob/main/sounds/simon_full.mp3';
-
-    //creating a new request
-    var request = new XMLHttpRequest();
-    request.open('GET', audioURL, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        //take the audio from http request and decode it in an audio buffer
-        context.decodeAudioData(request.response, function (buffer) {
-            // save buffer, to not load again
-            saved = buffer;
-            // play sound
-            playSound(buffer);
-        });
-    };
-    request.send();
+  var audioURL = '/sounds/simon_full.mp3';
+  var request = new XMLHttpRequest();
+  request.open('GET', audioURL, true);
+  request.responseType = 'arraybuffer';
+  request.onload = function () {
+    context.decodeAudioData(request.response, function (buffer) {
+      saved = buffer;
+    });
+  };
+  request.send();
 }
 
-function doIt() {
-  console.log(saved);
-  playSound(saved);
-};
-
-doIt();
-
-//playing the audio file
 function playSound(buffer) {
-    //creating source node
-    var source = context.createBufferSource();
-    //passing in data
-    source.buffer = buffer;
-    //giving the source which sound to play
-    source.connect(context.destination);
-    //start playing
-    source.start(0);
+  if (source) {
+    source.stop();
+    source = null;
+  };
+  source = context.createBufferSource();
+  source.buffer = buffer;
+  source.connect(context.destination);
+  source.start(0);
+}
+
+// document.querySelectorAll('.sound').forEach((element) => element.addEventListener('click', () => {
+//   if (!gameStarted) {
+//     gameStarted = true;
+try {
+  context = new (window.AudioContext || window.webkitAudioContext)();
+  context.resume();
+  loadSound();
+} catch(e) {
+  console.log('Narp.')
 }
 
 pads.forEach(pad => {
@@ -71,7 +67,10 @@ pads.forEach(pad => {
   fxSwitch.addEventListener('click', handleFXSwitchClick);
 });
 
-document.querySelectorAll('.pad-fx').forEach(fx => fx.volume = 0.5);
+// const AudioContext = window.AudioContext || window.webkitAudioContext || false;
+// console.log('AudioContext: ', AudioContext);
+
+// document.querySelectorAll('.pad-fx').forEach(fx => fx.volume = 0.5);
 
 innerCircle.addEventListener('click', startGame);
 
@@ -88,12 +87,14 @@ function handleFXSwitchClick() {
 }
 
 function handleMouseDown() {
+  return playSound(saved);
   if (playing) return;
   const number = Number(this.dataset.pad_number);
   if (gameStarted && number != sequence[sequenceIndex]) return gameOver();
   if (fxEnabled) {
-    fx.currentTime = Number(number);
-    fx.play();
+    // fx.currentTime = Number(number);
+    // fx.play();
+    // playSound();
     setInterval(() => {
       if (fx.currentTime.toFixed(1) == number + .6) fx.pause();
     }, 100)
